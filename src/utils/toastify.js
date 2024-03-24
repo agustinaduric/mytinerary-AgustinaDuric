ca506af7d350ed98d083bc08b6e411a2c9d0b746
agustinaduric <130220670+agustinaduric@users.noreply.github.com>
2024-03-23 23:35:59 -0300

    fix: auto-close time

--- src/utils/toastify.js
+++ src/utils/toastify.js
@@ -1,7 +1,7 @@
 import { toast, Bounce} from 'react-toastify';
 const success = (msg) =>toast.success(msg, {
     position: "top-right",
-    autoClose: 15000,
+    autoClose: 1500,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
